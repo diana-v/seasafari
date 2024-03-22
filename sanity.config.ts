1138acbca7f31c99a5f830524e408983009aa3c4
Diana Valaitytė <d.valaityte.97@gmail.com>
2024-03-22 16:46:49 +0200

    Fix sanity admin styling. Configure new dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,27 +7,52 @@ import { schemaTypes } from './src/studio/schemas';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default defineConfig({
-    name: 'default',
-    title: isProduction ? process.env.NEXT_PUBLIC_SANITY_TITLE : import.meta.env.SANITY_STUDIO_TITLE,
+export default defineConfig([
+    {
+        name: 'production-workspace',
+        title: isProduction ? process.env.NEXT_PUBLIC_SANITY_TITLE : import.meta.env.SANITY_STUDIO_TITLE,
 
-    projectId: isProduction ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: isProduction ? process.env.NEXT_PUBLIC_SANITY_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
+        projectId: isProduction ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
+        dataset: isProduction ? process.env.NEXT_PUBLIC_SANITY_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
+        basePath: '/studio/production',
 
-    plugins: [
-        structureTool(),
-        visionTool(),
-        documentInternationalization({
-            supportedLanguages: [
-                {id: 'lt', title: 'Lithuanian'},
-                {id: 'en', title: 'English'},
-                {id: 'ru', title: 'Russian'}
-            ],
-            schemaTypes: ['home', 'about', 'contact', 'offer', 'offers', 'gallery', 'faq', 'reviews', 'navigation', 'safety', 'blockContent', 'localeBlock', 'localeString'],
-        })
-    ],
+        plugins: [
+            structureTool(),
+            visionTool(),
+            documentInternationalization({
+                supportedLanguages: [
+                    {id: 'lt', title: 'Lithuanian'},
+                    {id: 'en', title: 'English'},
+                    {id: 'ru', title: 'Russian'}
+                ],
+                schemaTypes: ['home', 'about', 'contact', 'offer', 'offers', 'gallery', 'faq', 'reviews', 'navigation', 'safety', 'blockContent', 'localeBlock', 'localeString'],
+            })
+        ],
 
-    schema: {
-        types: schemaTypes,
+        schema: {
+            types: schemaTypes,
+        }
     },
-});
+    {
+        name: 'staging-workspace',
+        title: isProduction ? process.env.NEXT_PUBLIC_SANITY_TITLE : import.meta.env.SANITY_STUDIO_TITLE,
+        projectId: isProduction ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID : import.meta.env.SANITY_STUDIO_PROJECT_ID,
+        dataset: isProduction ? process.env.NEXT_PUBLIC_SANITY_DATASET : import.meta.env.SANITY_STUDIO_DATASET,
+        basePath: '/studio/staging',
+        plugins: [
+            structureTool(),
+            visionTool(),
+            documentInternationalization({
+                supportedLanguages: [
+                    {id: 'lt', title: 'Lithuanian'},
+                    {id: 'en', title: 'English'},
+                    {id: 'ru', title: 'Russian'}
+                ],
+                schemaTypes: ['home', 'about', 'contact', 'offer', 'offers', 'gallery', 'faq', 'reviews', 'navigation', 'safety', 'blockContent', 'localeBlock', 'localeString'],
+            })
+        ],
+        schema: {
+            types: schemaTypes,
+        },
+    }
+]);
