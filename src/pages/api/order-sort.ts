b97d32b0fe8b1d62d1c9e8165f54fc34197b4299
Diana Valaitytė <d.valaityte.97@gmail.com>
2024-06-08 22:37:07 +0300

    Add filtering by status

--- src/pages/api/order-sort.ts
+++ src/pages/api/order-sort.ts
@@ -1,18 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { asc, desc, ilike, or } from 'drizzle-orm';
+import { asc, desc, ilike, or, and, eq } from 'drizzle-orm';
 
 import { db } from '@/server/db';
-import { Order, orders } from '@/server/db/schema';
+import { Order, orders, Status } from '@/server/db/schema';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { searchTerm, field, direction } = req.query;
+    const { searchTerm, field, direction, showCompleted } = req.query;
 
     try {
         const sortDirection = direction === 'asc' ? asc : desc;
+        const shouldShowCompleted = showCompleted === 'true';
 
         const sortedOrders = await db.query.orders.findMany({
-            where: (order) => or(ilike(order.orderRef, `%${searchTerm}%`), ilike(order.orderEmail, `%${searchTerm}%`)),
-            orderBy: [sortDirection(orders[field as keyof Order])],
+            where: (order) =>
+                and(
+                    or(ilike(order.orderRef, `%${searchTerm}%`), ilike(order.orderEmail, `%${searchTerm}%`)),
+                    shouldShowCompleted
+                        ? or(eq(order.status, Status.COMPLETED), eq(order.status, Status.CREATED))
+                        : eq(order.status, Status.CREATED)
+                ),
+            orderBy: sortDirection(orders[field as keyof Order]),
         });
 
         return res.status(200).send(sortedOrders);
