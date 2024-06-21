aecc62e391e04995e7330053d58d0af21dae8cd7
Diana Valaitytė <d.valaityte.97@gmail.com>
2024-06-21 22:54:28 +0300

    Add pdf attachment to order condirmation email

---
+++ src/translations/paymentSuccessPdf.ts
@@ -0,0 +1,89 @@
+export type LocaleType = 'en' | 'lt' | 'ru';
+
+export const languages = {
+    en: {
+        giftCard: 'Gift Certificate',
+        title: 'Extreme boat ride in the Baltic Sea',
+        about: 'About your gift:',
+        description:
+            'The high-speed R.I.B. boat and the waves of the Baltic Sea will not disappoint even the biggest adrenaline fans. This is an extreme ride full of unforgettable experiences and laughter. During the trip, you will have the opportunity to sail along the Danė River, see the gates of the Baltic Sea. So fasten your life jackets and get comfortable and enjoy the carefree day full of adrenaline!',
+        intentTitle: 'The gift is for:',
+        intentDescription: 'The number of persons is specified in the title',
+        registrationTitle: 'Registration:',
+        registrationDescription: '+37067572133',
+        locationTitle: 'Location:',
+        locationDescription:
+            '"Sea Safari", Danė River, Klaipėda (the exact location will be specified by the organizer)',
+        durationTitle: 'Service duration:',
+        durationDescription: '30min',
+        validityTitle: 'Validity period:',
+        validityDescription: '12 months',
+        giftCardRef: 'Order Ref:',
+        validFrom: 'Valid From:',
+        validTo: 'Valid To:',
+        otherInfoTitle: 'Other important information:',
+        otherInfoDescription:
+            'The service is provided during the warm season and in autumn, when it is not too cold. The service is provided on Fridays-Sundays. During the trip, participants may get splashed with water. If necessary, waterproof suits are provided in autumn, and raincoats on rainy summer days. The service is provided for a group of 8 people.',
+        disclaimer1: 'The gift certificate is not refundable or exchangeable for cash.',
+        disclaimer2:
+            'No change is given when using the gift certificate if the service amount is less than the nominal value of the gift certificate.',
+        disclaimer3:
+            'If the gift certificate is not used within the validity period, it is considered that the service has been provided. Money for an unused gift certificate is not refunded.',
+    },
+    lt: {
+        giftCard: 'Dovanų Kuponas',
+        title: 'Ekstremalus plaukimas kateriu Baltijos jūroje',
+        about: 'Apie Jūsų dovaną:',
+        description:
+            'Greitaeigis R.I.B. kateris ir Baltijos jūros bangų mūša nenuvils net ir didžiausių adrenalino fanatikų. Tai neišdildomų įspūdžių ir juoko kupinas ekstremalus plaukimas. Kelionės metu turėsite galimybę praplaukti Danės upe, pamatyti Baltijos jūros vartus. Tad užsisekite liemenes ir įsipatoginkite ir mėgaukitės nerūpestingos dienos šėlsmu ir adrenalino potyriais!',
+        intentTitle: 'Dovana skirta:',
+        intentDescription: 'Asmenų skaičius nurodytas pavadinime',
+        registrationTitle: 'Registracija:',
+        registrationDescription: '+37067572133',
+        locationTitle: 'Vieta:',
+        locationDescription: '"Sea Safari", Danės upė, Klaipėda (tikslią vietą patikslins pramogos organizatorius)',
+        durationTitle: 'Paslaugos trukmė:',
+        durationDescription: '30min',
+        validityTitle: 'Galiojimo trukmė:',
+        validityDescription: '12 mėn',
+        giftCardRef: 'Kupono kodas:',
+        validFrom: 'Galioja nuo:',
+        validTo: 'Galioja iki:',
+        otherInfoTitle: 'Kita svarbi informacija:',
+        otherInfoDescription:
+            'Paslauga teikiama šiltuoju metų laiku ir rudenį, kai nėra per daug šalta. Paslauga teikiama penktadieniais-sekmadieniais. Kelionės metu pramogos dalyviai gali būti aptaškyti vandeniu. Esant poreikiui, rudenį suteikiami neperšlampami hidrokostiumai, o lietingomis vasaros dienomis – lietpalčiai. Paslauga teikiama surinkus 8 asm. grupę.',
+        disclaimer1: 'Dovanų čekis nėra gražinamas ar keičiamas į pinigus.',
+        disclaimer2:
+            'Naudojant dovanų čekį nėra duodama grąža, jei paslaugų suma nesiekia dovanų čekio nominalios vertės',
+        disclaimer3:
+            'Jei dovanų čekis nebuvo panaudotas jo galiojimo termino laikotarpiu, yra laikoma kad paslauga yra suteikta. Pinigai už nepanaudotą dovanų čekį nėra grąžinami.',
+    },
+    ru: {
+        giftCard: 'Подарочный Сертификат',
+        title: 'Экстремальная поездка на катере по Балтийскому морю',
+        about: 'О вашем подарке:',
+        description:
+            'Высокоскоростной катер R.I.B. и волны Балтийского моря не разочаруют даже самых больших любителей адреналина. Это экстремальная поездка, полная незабываемых впечатлений и смеха. Во время поездки у вас будет возможность проплыть по реке Дане, увидеть ворота Балтийского моря. Так что пристегните спасательные жилеты, устройтесь поудобнее и наслаждайтесь беззаботным днем, полным адреналина!',
+        intentTitle: 'Подарок предназначен:',
+        intentDescription: 'Количество человек указано в названии',
+        registrationTitle: 'Регистрация:',
+        registrationDescription: '+37067572133',
+        locationTitle: 'Место проведения:',
+        locationDescription: '"Морское Сафари", река Дане, Клайпеда (точное место будет указано организатором)',
+        durationTitle: 'Продолжительность услуги:',
+        durationDescription: '30мин',
+        validityTitle: 'Срок действия:',
+        validityDescription: '12 месяцев',
+        giftCardRef: 'Номер заказа:',
+        validFrom: 'Действительно с:',
+        validTo: 'Действительно до:',
+        otherInfoTitle: 'Другая важная информация:',
+        otherInfoDescription:
+            'Услуга предоставляется в теплое время года и осенью, когда не слишком холодно. Услуга предоставляется по пятницам-воскресеньям. Во время поездки участники могут намокнуть. При необходимости осенью предоставляются водонепроницаемые костюмы, а в дождливые летние дни - дождевики. Услуга предоставляется для группы из 8 человек.',
+        disclaimer1: 'Подарочный сертификат не подлежит возврату или обмену на деньги.',
+        disclaimer2:
+            'Сдача не выдается при использовании подарочного сертификата, если сумма услуги меньше номинальной стоимости подарочного сертификата.',
+        disclaimer3:
+            'Если подарочный сертификат не использован в течение срока его действия, считается, что услуга была предоставлена. Деньги за неиспользованный подарочный сертификат не возвращаются.',
+    },
+};
