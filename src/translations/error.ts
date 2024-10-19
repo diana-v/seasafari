dd71048ae3721bfb303a22d6301ec236e420e877
diana-v <55509483+diana-v@users.noreply.github.com>
2024-10-19 16:56:06 +0300

    Handle duplicate refs and implement error page (#57) (#58)
    * Handle duplicate order refs from being added to db. Implement error page
    
    * Handle no errorCode being passed to error page in query params
    
    * Fix lint

---
+++ src/translations/error.ts
@@ -0,0 +1,28 @@
+export type LocaleType = 'en' | 'lt' | 'ru';
+
+export const languages = {
+    en: {
+        errorMessage: 'There was an error processing your request. Please try again later.',
+        disclaimerQuestion: 'Do you have any questions or need support? ',
+        disclaimerSubmitForm: 'Submit a contact form ',
+        disclaimerOr: 'or reach out at ',
+        disclaimerSendEmail: 'seasafari.lietuva@gmail.com',
+        link: 'Back to Home',
+    },
+    lt: {
+        errorMessage: 'Apdorojant jūsų užklausą įvyko klaida. Prašome bandyti vėliau.',
+        disclaimerQuestion: 'Turite klausimų ar reikia pagalbos? ',
+        disclaimerSubmitForm: 'Užpildykite kontaktinę formą ',
+        disclaimerOr: 'arba susisiekite su mumis adresu ',
+        disclaimerSendEmail: 'seasafari.lietuva@gmail.com',
+        link: 'Grįžti į pagrindinį puslapį',
+    },
+    ru: {
+        errorMessage: 'При обработке вашего запроса произошла ошибка. Пожалуйста, попробуйте позже.',
+        disclaimerQuestion: 'Не получили электронное письмо или есть другие вопросы? ',
+        disclaimerSubmitForm: 'Отправьте контактную форму ',
+        disclaimerOr: 'или свяжитесь с нами по ',
+        disclaimerSendEmail: 'seasafari.lietuva@gmail.com',
+        link: 'Вернуться на главную',
+    },
+};
