6c6594e7d6443996ad282ed5efd9497468f1674c
diana-v <55509483+diana-v@users.noreply.github.com>
2025-11-02 10:53:39 +0200

    Use correct endpoint (#109)
    * Fix passing of key, add translations for ssr response
    
    * Fix lint
    
    * Use correct endpoint

--- src/pages/api/scan-qr-proxy.ts
+++ src/pages/api/scan-qr-proxy.ts
@@ -5,18 +5,27 @@ import { isString } from '@/utils/isString';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { token, locale } = req.query;
 
-    if (!isString(token)) {
+    if (!isString(token) || !isString(locale)) {
         return res.status(400).send('Invalid query parameters');
     }
 
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_DOMAIN}/api/verify-qr?token=${encodeURIComponent(token)}&locale=${locale}`,
-        {
-            headers: { 'x-api-key': process.env.ADMIN_API_KEY } as HeadersInit,
-        }
-    );
+    try {
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_DOMAIN}/api/verify-qr?token=${encodeURIComponent(token)}
+            &locale=${encodeURIComponent(locale)}`,
+            {
+                headers: {
+                    'x-api-key': process.env.ADMIN_API_KEY ?? '',
+                } as HeadersInit,
+            }
+        );
 
-    const data = await response.json();
+        const data = await response.json();
 
-    res.status(response.status).json(data);
+        return res.status(response.status).json(data);
+    } catch (error) {
+        console.error('Failed to fetch verify-qr response:', error);
+
+        return res.status(500).json({ valid: false, reason: 'Server error while verifying QR' });
+    }
 }
