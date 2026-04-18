faa734a305040ad0626d7297a48a554dcf143e42
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-18 22:22:23 +0300

    disable turbopack for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 
     // Run your local dev server before starting the tests
     webServer: {
-        command: 'npm run dev',
+        command: 'npm run dev:test',
         reuseExistingServer: !process.env.CI,
         stderr: 'pipe',
         stdout: 'ignore',
