b45a01c5f0913b40d477c07aa801dfd08ebf7b89
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-18 23:02:12 +0300

    attempt fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,22 +1,21 @@
 import dotenv from 'dotenv'
 
-const isProduction = process.env.NODE_ENV === 'production'
-const { parsed } = dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env.local' });
 
 const connectionString = [
   'postgresql://',
-  isProduction ? process.env.PGUSER : parsed?.PGUSER,
+    process.env.PGUSER,
   ':',
-  isProduction ? process.env.PGPASSWORD : parsed?.PGPASSWORD,
+    process.env.PGPASSWORD,
   '@',
-  isProduction ? process.env.PGHOST : parsed?.PGHOST,
+    process.env.PGHOST,
   '/',
-  isProduction ? process.env.PGDATABASE : parsed?.PGDATABASE,
-  '?sslmode=require',
+    process.env.PGDATABASE,
+    '?sslmode=require',
 ].join('');
 
 export default {
-  schema:'./src/server/db/schema.ts',
-  driver:'pg',
-  dbCredentials: { connectionString },
+    dbCredentials: { connectionString },
+    driver: 'pg',
+    schema: './src/server/db/schema.ts',
 };
