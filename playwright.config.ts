a42b6ca87f76b9f54196adb1b554e284ccea59a7
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-18 22:59:22 +0300

    revert

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 
     // Run your local dev server before starting the tests
     webServer: {
-        command: 'npm run start',
+        command: 'npm run dev',
         reuseExistingServer: !process.env.CI,
         stderr: 'pipe',
         stdout: 'ignore',
