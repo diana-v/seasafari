de6ec65005925be24c92298fde684474de79e126
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-19 11:18:53 +0300

    Add checks

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -5,26 +5,22 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import * as schema from './schema';
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_ENV === 'test';
 const { parsed } = dotenv.config({ path: '.env.local' });
+const showParsed = !(isProduction || isTest)
 
 const connectionString = [
     'postgresql://',
-    isProduction ? process.env.PGUSER : parsed?.PGUSER,
+    showParsed ? parsed?.PGUSER : process.env.PGUSER,
     ':',
-    isProduction ? process.env.PGPASSWORD : parsed?.PGPASSWORD,
+    showParsed ? parsed?.PGPASSWORD : process.env.PGPASSWORD,
     '@',
-    isProduction ? process.env.PGHOST : parsed?.PGHOST,
+    showParsed ? parsed?.PGHOST : process.env.PGHOST,
     '/',
-    isProduction ? process.env.PGDATABASE : parsed?.PGDATABASE,
+    showParsed ? parsed?.PGDATABASE : process.env.PGDATABASE,
     '?sslmode=require',
 ].join('');
 
 const pool = new Pool({ connectionString });
-
-console.log('🔥 DB INIT ENV CHECK');
-console.log('PGHOST', process.env.PGHOST);
-console.log('PGUSER', process.env.PGUSER);
-console.log('PGDATABASE', process.env.PGDATABASE);
-console.log('PGPASSWORD length', process.env.PGPASSWORD?.length);
 
 export const db = drizzle(pool, { schema });
