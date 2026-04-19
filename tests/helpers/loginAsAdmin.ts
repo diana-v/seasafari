543ba746388a36fa161a9d3b6acb45a4ffcef26a
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-19 14:31:57 +0300

    fix test

--- tests/helpers/loginAsAdmin.ts
+++ tests/helpers/loginAsAdmin.ts
@@ -4,10 +4,10 @@ import { BrowserContext } from '@playwright/test';
 export async function loginAsAdmin(context: BrowserContext, user: string, pass: string) {
     await context.addCookies([
         {
-            domain: '127.0.0.1',
             httpOnly: true,
             name: 'auth',
             path: '/',
+            url: 'http://127.0.0.1:3000',
             value: Buffer.from(`${user}:${pass}`).toString('base64'),
         },
     ]);
