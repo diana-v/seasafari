e6b931bd2b74c2c4f595117267bdcdc9aed322f2
Diana Valaityte <d.valaityte.97@gmail.com>
2026-04-19 11:55:09 +0300

    add env

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,6 +72,7 @@ export default defineConfig({
     // Run your local dev server before starting the tests
     webServer: {
         command: 'npm run dev',
+        env: process.env as Record<string, string>,
         reuseExistingServer: !process.env.CI,
         stderr: 'pipe',
         stdout: 'ignore',
